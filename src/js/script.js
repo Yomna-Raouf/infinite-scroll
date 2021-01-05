3d7fef142ebbb4f83cdb4dc38c297ff214ed1918
yomna raouf <yomnaraouf99@gmail.com>
2021-01-04 23:57:56 -0800

    fetch data from api

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,23 @@
-console.log("infinite-scroll");
 import "../style/style.scss";
+import '@fortawesome/fontawesome-free/js/all.min';
+
+// Unsplash API
+const count = 10;
+const collectionId = 190728;
+const apiKey = "U1S2KhTzek0Eb5UBFbP9HVnY7UnFfq7j-7eCC3101W8";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}&collection=${collectionId}`;
+
+// Fetch Data From API
+
+const fetchPictures = async () => {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// On Load
+// fetchPictures();
